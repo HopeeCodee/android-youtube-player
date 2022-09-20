b25af488cc525e633ebc18c3c6616f0a107b750d
muazka <30705836+muazka@users.noreply.github.com>
2022-09-20 16:45:39 +0300

    Update YoutubePlayerQualityListener.kt

--- core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/player/listeners/YoutubePlayerQualityListener.kt
+++ core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/player/listeners/YoutubePlayerQualityListener.kt
@@ -1,1 +1,6 @@
+package com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners
 
+interface YouTubePlayerQualityListener {
+    fun onYouTubePlayerQualityChange()
+    fun onYouTubePlayerAutomateQuality()
+}
