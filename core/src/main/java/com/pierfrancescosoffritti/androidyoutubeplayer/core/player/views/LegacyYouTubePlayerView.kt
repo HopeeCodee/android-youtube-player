65dba0552ffb14a1b9d04a1dcf88b17f374a88c3
muazka <30705836+muazka@users.noreply.github.com>
2022-09-22 17:28:41 +0300

    Update LegacyYouTubePlayerView.kt

--- core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/player/views/LegacyYouTubePlayerView.kt
+++ core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/player/views/LegacyYouTubePlayerView.kt
@@ -32,7 +32,7 @@ internal class LegacyYouTubePlayerView(context: Context, attrs: AttributeSet? = 
     constructor(context: Context): this(context, null, 0)
     constructor(context: Context, attrs: AttributeSet? = null): this(context, attrs, 0)
 
-    internal val youTubePlayer: WebViewYouTubePlayer = WebViewYouTubePlayer(context)
+    val youTubePlayer: WebViewYouTubePlayer = WebViewYouTubePlayer(context)
     private val defaultPlayerUiController: DefaultPlayerUiController
 
     private val networkListener = NetworkListener()
