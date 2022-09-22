1a715381f376e04483fc3d7cb7a32cde93f86181
muazka <30705836+muazka@users.noreply.github.com>
2022-09-22 21:53:57 +0300

    Update WebViewYouTubePlayer.kt

--- core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/player/views/WebViewYouTubePlayer.kt
+++ core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/player/views/WebViewYouTubePlayer.kt
@@ -68,7 +68,6 @@ internal class WebViewYouTubePlayer constructor(context: Context, attrs: Attribu
     
     override fun setQuality(playbackQuality: String) {
         mainThreadHandler.post { loadUrl("javascript:setQuality($playbackQuality)") }
-        mainThreadHandler.post { loadUrl("javascript:setVolume(0)") }
     }
 
     override fun seekTo(time: Float) {
