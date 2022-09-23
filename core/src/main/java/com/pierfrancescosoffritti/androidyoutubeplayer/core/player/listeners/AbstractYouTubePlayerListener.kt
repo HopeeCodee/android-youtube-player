0d9d301d88ceeb7036cd01f3a8cd126884143cf9
muazka <30705836+muazka@users.noreply.github.com>
2022-09-23 19:56:31 +0300

    Update AbstractYouTubePlayerListener.kt

--- core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/player/listeners/AbstractYouTubePlayerListener.kt
+++ core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/player/listeners/AbstractYouTubePlayerListener.kt
@@ -17,4 +17,5 @@ abstract class AbstractYouTubePlayerListener : YouTubePlayerListener {
     override fun onVideoDuration(youTubePlayer: YouTubePlayer, duration: String) {}
     override fun onVideoLoadedFraction(youTubePlayer: YouTubePlayer, loadedFraction: Float) {}
     override fun onVideoId(youTubePlayer: YouTubePlayer, videoId: String) {}
+    override fun onVideoQualities(youTubePlayer: YouTubePlayer, qualityOptions: String) { }
 }
