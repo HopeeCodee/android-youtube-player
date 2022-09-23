5da07333fe9a54fc175988065ea598c462a52c2d
muazka <30705836+muazka@users.noreply.github.com>
2022-09-23 19:53:44 +0300

    Update YouTubePlayerSeekBar.kt

--- core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/ui/views/YouTubePlayerSeekBar.kt
+++ core/src/main/java/com/pierfrancescosoffritti/androidyoutubeplayer/core/ui/views/YouTubePlayerSeekBar.kt
@@ -146,6 +146,7 @@ class YouTubePlayerSeekBar(context: Context, attrs: AttributeSet? = null): Linea
 
     override fun onReady(youTubePlayer: YouTubePlayer) { }
     override fun onVideoId(youTubePlayer: YouTubePlayer, videoId: String) { }
+    override fun onVideoQualities(youTubePlayer: YouTubePlayer, qualityOptions: String) { }
     override fun onApiChange(youTubePlayer: YouTubePlayer) { }
     override fun onPlaybackQualityChange(youTubePlayer: YouTubePlayer, playbackQuality: PlayerConstants.PlaybackQuality) { }
     override fun onPlaybackRateChange(youTubePlayer: YouTubePlayer, playbackRate: PlayerConstants.PlaybackRate) { }
